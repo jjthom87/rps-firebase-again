ec5a1a2aca528600be30541c395a4002c8a6441e
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-12-11 17:07:14 -0500

    getting there

--- index.js
+++ index.js
@@ -11,62 +11,71 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var playerOneJoined;
-var playerTwoJoined;
-
 firebase.database().ref("playerOneJoined").set(false);
 firebase.database().ref("playerTwoJoined").set(false);
-firebase.database().ref("amountOfPlayers").set(0);
-firebase.database().ref("gameStarted").set(false);
+
+firebase.database().ref("playerOneChoice").set(null);
+firebase.database().ref("playerTwoChoice").set(null);
 
 firebase.database().ref("playerOneJoined").on("value", function(snapshot){
   if(snapshot.val()){
-    $("#player-one-join-game-button").attr("disabled", true);
-    $("#player-one-join-game-button").css("cursor", "default");
-    $("#player-one-join-game-button").css("box-shadow", "none");
+    defaultDisabledButton("#player-one-join-game-button")
   } else {
-    $("#player-one-join-game-button").attr("disabled", false);
-    $(".choices").empty();
+    enableJoinButton("#player-one-join-game-button")
   }
 })
 
 firebase.database().ref("playerTwoJoined").on("value", function(snapshot){
   if(snapshot.val()){
-    $("#player-two-join-game-button").attr("disabled", true)
-    $("#player-two-join-game-button").css("cursor", "default");
-    $("#player-two-join-game-button").css("box-shadow", "none");
+    defaultDisabledButton("#player-one-join-game-button")
 
     $(".player-two-game-div > p").text("Player Two Has Joined")
   } else {
-    $("#player-two-join-game-button").attr("disabled", false)
-    $(".choices").empty();
+    enableJoinButton("#player-two-join-game-button")
   }
 
 })
 
 $("#player-one-join-game-button").click(function(){
   firebase.database().ref("playerOneJoined").set(true)
-  populateChoicesForPlayer("#player-one-choices");
+  populateChoicesForPlayer("#player-one-choices", "player-one");
   appendWaitingContent();
 
-  ifBothPlayersJoined();
+  bothPlayerStatuses();
 });
 
 $("#player-two-join-game-button").click(function(){
   firebase.database().ref("playerTwoJoined").set(true)
-  populateChoicesForPlayer("#player-two-choices");
+  populateChoicesForPlayer("#player-two-choices", "player-two");
 
-  ifBothPlayersJoined();
+  bothPlayerStatuses();
 });
 
-ifBothPlayersJoined();
+bothPlayerStatuses();
 
-var populateChoicesForPlayer = (choicesDiv) => {
+$(document).on("click", ".player-one-choice-buttons", function(){
+  var playerOneChoice = $(this).data("choice");
+
+  firebase.database().ref("playerOneChoice").set(playerOneChoice)
+
+  bothPlayerChoices()
+})
+
+$(document).on("click", ".player-two-choice-buttons", function(){
+  var playerTwoChoice = $(this).data("choice");
+
+  firebase.database().ref("playerTwoChoice").set(playerTwoChoice)
+
+  bothPlayerChoices()
+})
+
+var populateChoicesForPlayer = (choicesDiv, player) => {
   var choices = ['rock', 'paper', 'scissor'];
 
   for(var i = 0; i < choices.length; i++){
     var p = $("<p>");
     var choiceButton = $("<button>");
+    choiceButton.addClass("choice-buttons " + player + "-choice-buttons")
     choiceButton.attr("disabled", true)
     choiceButton.text(choices[i].charAt(0).toUpperCase() + choices[i].substring(1, choices[i].length))
     choiceButton.attr("data-choice", choices[i]);
@@ -91,21 +100,88 @@ var appendJoinedContent = () => {
   $(".player-one-game-div").append(p)
 }
 
-function ifBothPlayersJoined(){
+function bothPlayerStatuses(){
   var playerOneJoined = false;
   var playerTwoJoined = false;
 
   firebase.database().ref("playerOneJoined").on("value", function(playerOneShapshot){
-    console.log(playerOneShapshot.val());
     playerOneJoined = playerOneShapshot.val();
+
     firebase.database().ref("playerTwoJoined").on("value", function(playerTwoShapshot){
-      console.log(playerTwoShapshot.val());
       playerTwoJoined = playerTwoShapshot.val();
+
       if(playerOneJoined && playerTwoJoined){
         $(".choices > button").attr("disabled", false);
+      } else if (!playerOneJoined && !playerTwoJoined){
+        $("#player-two-join-game-button").attr("disabled", true)
+      } else if (playerOneJoined && !playerTwoJoined){
+        $("#player-two-join-game-button").attr("disabled", false)
       }
 
     })
   })
 
+}
+
+function defaultDisabledButton(playerButton){
+  $(playerButton).attr("disabled", true);
+  $(playerButton).css("cursor", "default");
+  $(playerButton).css("box-shadow", "none");
+}
+
+function enableJoinButton(playerButton){
+  $(playerButton).attr("disabled", false)
+  $(".choices").empty();
+}
+
+function bothPlayerChoices(){
+  var playerOneChoice = null;
+  var playerTwoChoice = null;
+
+  firebase.database().ref("playerOneChoice").on("value", function(playerOneShapshot){
+    playerOneChoice = playerOneShapshot.val();
+
+    firebase.database().ref("playerTwoChoice").on("value", function(playerTwoShapshot){
+      playerTwoChoice = playerTwoShapshot.val();
+
+      if(playerOneChoice && playerTwoChoice){
+        console.log("IN HERE BITCH")
+      } else if (playerOneChoice && !playerTwoChoice){
+
+      } else if (!playerOneChoice && playerTwoChoice){
+
+      } else {
+
+      }
+
+    });
+  });
+}
+
+function gameLogic(playerOneGuess, playerTwoGuess){
+  if(playerOneGuess == "paper" && playerTwoGuess == "paper"){
+    game.ties++;
+    game.roundWinner = "Tie";
+  } else if (playerOneGuess == "rock" && playerTwoGuess == "rock"){
+    game.ties++;
+    game.roundWinner = "Tie";
+  } else if (playerOneGuess == "scissor" && playerTwoGuess == "scissor"){
+    game.ties++;
+    game.roundWinner = "Tie";
+  } else if (playerOneGuess == "paper" && playerTwoGuess == "rock"){
+    player.wins++;
+    game.roundWinner = "Player";
+  } else if (playerOneGuess == "rock" && playerTwoGuess == "scissor"){
+    player.wins++;
+    game.roundWinner = "Player";
+  } else if (playerOneGuess == "scissor" && playerTwoGuess == "paper"){
+    player.wins++;
+    game.roundWinner = "Player";
+  } else if (playerOneGuess == "rock" && playerTwoGuess == "paper"){
+    game.roundWinner = "Computer";
+  } else if (playerOneGuess == "paper" && playerTwoGuess == "scissor"){
+    game.roundWinner = "Computer";
+  } else if (playerOneGuess == "scissor" && playerTwoGuess == "rock"){
+    game.roundWinner = "Computer";
+  }
 }
